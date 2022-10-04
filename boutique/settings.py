deae597daed08eab06c150d1ea27efa68184d4b5
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-10-04 20:10:08 +0300

    Update settings.py

--- boutique/settings.py
+++ boutique/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
